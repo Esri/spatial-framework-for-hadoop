d9fd051121ec429eeedeed9e30a9962562592284
Mike Park <climbage@gmail.com>
2013-03-07 12:27:29 -0800

    add sql file to automate sample

---
+++ sample-workflows/HiveAggregationSample/run-sample.sql
@@ -0,0 +1,20 @@
+add jar
+  ${env:HOME}/esri-git/hadoop-tools/sample-workflows/Java/esri-geometry-api.jar
+  ${env:HOME}/esri-git/hadoop-tools/sample-workflows/Java/hadoop-utilities.jar;
+
+CREATE EXTERNAL TABLE IF NOT EXISTS earthquakes (earthquake_date STRING, latitude DOUBLE, longitude DOUBLE, magnitude DOUBLE)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+LOCATION '${env:HOME}/esri-git/hadoop-tools/sample-workflows/data/earthquake-data';
+
+CREATE EXTERNAL TABLE IF NOT EXISTS counties (Area string, Perimeter string, State string, County string, Name string, BoundaryShape binary)                                         
+ROW FORMAT SERDE 'com.esri.hadoop.hive.serde.JsonSerde'              
+STORED AS INPUTFORMAT 'com.esri.hadoop.hive.serde.EnclosedJsonInputFormat'
+OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION '${env:HOME}/esri-git/hadoop-tools/sample-workflows/data/counties-data';
+
+SELECT counties.name, count(*) cnt FROM counties
+JOIN earthquakes
+WHERE ST_Contains(counties.boundaryshape, ST_Point(earthquakes.longitude, earthquakes.latitude))
+GROUP BY counties.name
+ORDER BY cnt desc;
+
