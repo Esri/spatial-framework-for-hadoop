56f0ff4fc21cdc5183d38e8896e64ef0aee6baeb
Mike Park <climbage@gmail.com>
2014-06-13 11:20:34 -0700

    updated ST_Contains to generic

--- hive/src/main/java/com/esri/hadoop/hive/ST_Contains.java
+++ hive/src/main/java/com/esri/hadoop/hive/ST_Contains.java
@@ -1,48 +1,95 @@
 package com.esri.hadoop.hive;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hive.ql.exec.Description;
-import org.apache.hadoop.io.BooleanWritable;
-import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.UDFType;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.log4j.Logger;
 
+import com.esri.core.geometry.OperatorContains;
+import com.esri.core.geometry.Geometry.GeometryAccelerationDegree;
 import com.esri.core.geometry.ogc.OGCGeometry;
 
+@UDFType(deterministic = true)
 @Description(
-	name = "ST_Contains",
-	value = "_FUNC_(geometry1, geometry2) - return true if geometry1 contains geometry2",
-	extended = "Example:\n" + 
-	"SELECT _FUNC_(st_polygon(1,1, 1,4, 4,4, 4,1), st_point(2, 3) from src LIMIT 1;  -- return true\n" + 
-	"SELECT _FUNC_(st_polygon(1,1, 1,4, 4,4, 4,1), st_point(8, 8) from src LIMIT 1;  -- return false"	
-	)
+		name = "ST_Contains",
+		value = "_FUNC_(geometry1, geometry2) - return true if geometry1 contains geometry2",
+		extended = "Example:\n" + 
+		"SELECT _FUNC_(st_polygon(1,1, 1,4, 4,4, 4,1), st_point(2, 3) from src LIMIT 1;  -- return true\n" + 
+		"SELECT _FUNC_(st_polygon(1,1, 1,4, 4,4, 4,1), st_point(8, 8) from src LIMIT 1;  -- return false"	
+		)
+public class ST_Contains extends GenericUDF {
 
-public class ST_Contains extends ST_GeometryRelational {
+	private static Logger LOG = Logger.getLogger(ST_ContainsGeneric.class);
+	
+	private static final int NUM_ARGS = 2;
+	private static final int GEOM_1 = 0;
+	private static final int GEOM_2 = 1;
+	
+	private transient HiveGeometryOIHelper geomHelper1;
+	private transient HiveGeometryOIHelper geomHelper2;
+	
+	private transient OperatorContains opContains = OperatorContains.local();
+	private transient boolean firstRun = true;
+	
+	public ST_Contains() {
+		
+	}
+	
+	@Override
+	public ObjectInspector initialize(ObjectInspector[] OIs)
+			throws UDFArgumentException {
 
-	static final Log LOG = LogFactory.getLog(ST_Contains.class.getName());
-	final BooleanWritable resultBoolean = new BooleanWritable();
-
-	public BooleanWritable evaluate(BytesWritable geometryref1, BytesWritable geometryref2)
-	{
-		if (geometryref1 == null || geometryref2 == null ||
-		    geometryref1.getLength() == 0 || geometryref2.getLength() == 0) {
-			LogUtils.Log_ArgumentsNull(LOG);
-			return null;
+		if (OIs.length != NUM_ARGS) {
+			throw new UDFArgumentException("The function ST_Contains requires 2 arguments.");
 		}
 		
-		if (!GeometryUtils.compareSpatialReferences(geometryref1, geometryref2)) {
-			LogUtils.Log_SRIDMismatch(LOG, geometryref1, geometryref2);
-			return null;
+		geomHelper1 = HiveGeometryOIHelper.create(OIs[GEOM_1], GEOM_1);
+		geomHelper2 = HiveGeometryOIHelper.create(OIs[GEOM_2], GEOM_2);
+		
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("OI[0]=" + geomHelper1);
+			LOG.debug("OI[1]=" + geomHelper2);
 		}
 
-		OGCGeometry ogcGeom1 = GeometryUtils.geometryFromEsriShape(geometryref1);
-		OGCGeometry ogcGeom2 = GeometryUtils.geometryFromEsriShape(geometryref2);
-		if (ogcGeom1 == null || ogcGeom2 == null){
-			LogUtils.Log_ArgumentsNull(LOG);
-			return null;
+		firstRun = true;
+		
+		return PrimitiveObjectInspectorFactory.javaBooleanObjectInspector;
+	}
+	
+	@Override
+	public Object evaluate(DeferredObject[] args) throws HiveException {
+		
+		OGCGeometry geom1 = geomHelper1.getGeometry(args);
+		OGCGeometry geom2 = geomHelper2.getGeometry(args);
+		
+		if (geom1 == null || geom2 == null) {
+			return false;
 		}
+		
+		if (firstRun) {
+			if (geomHelper1.isConstant()) {
+				opContains.accelerateGeometry(geom1.getEsriGeometry(), 
+						geom1.getEsriSpatialReference(), GeometryAccelerationDegree.enumMedium);
+				LOG.info("Accelerating geometry1");
+			}
 
-		resultBoolean.set(ogcGeom1.contains(ogcGeom2));
-		return resultBoolean;
+			firstRun = false;
+		}
+		
+		return opContains.execute(geom1.getEsriGeometry(), geom2.getEsriGeometry(), geom1.getEsriSpatialReference(), null);
 	}
 
+	public void close() {
+		OperatorContains.deaccelerateGeometry(geomHelper1.getConstantGeometry().getEsriGeometry());
+	}
+	
+	@Override
+	public String getDisplayString(String[] args) {
+		return String.format("returns true if %s contains %s", args[0], args[1]);
+	}
 }
+
