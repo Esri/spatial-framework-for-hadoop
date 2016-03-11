8b778ab75dbb944372cab644b57b769635508761
Randall Whitman <rwhitman@esri.com>
2016-03-11 09:29:17 -0800

    UnenclosedGeoJsonInputFormat: fix NPE with Hive-0.14  (#99)

--- json/src/main/java/com/esri/json/hadoop/UnenclosedGeoJsonInputFormat.java
+++ json/src/main/java/com/esri/json/hadoop/UnenclosedGeoJsonInputFormat.java
@@ -15,7 +15,7 @@ public class UnenclosedGeoJsonInputFormat extends FileInputFormat<LongWritable, 
 
 	// Mrv1 implementation member will be used only for getSplits().
     // Will be instantiated only when Mrv1 in use.
-	private org.apache.hadoop.mapred.TextInputFormat ifmtMrv1 = null;
+	private org.apache.hadoop.mapred.FileInputFormat<LongWritable,Text> ifmtMrv1 = null;
 
 	@Override
 	public RecordReader<LongWritable, Text> createRecordReader(InputSplit arg0, TaskAttemptContext arg1)
@@ -36,7 +36,15 @@ public class UnenclosedGeoJsonInputFormat extends FileInputFormat<LongWritable, 
 			org.apache.hadoop.mapred.JobConf arg0,
 			int arg1) throws IOException {
 		ifmtMrv1 = (ifmtMrv1!=null) ? ifmtMrv1 :
-		  new org.apache.hadoop.mapred.TextInputFormat();
+		  new org.apache.hadoop.mapred.FileInputFormat<LongWritable,Text>() {
+            // Dummy method to satisfy interface but not meant to be called
+			public org.apache.hadoop.mapred.RecordReader<LongWritable, Text> getRecordReader(
+			  org.apache.hadoop.mapred.InputSplit ign0,
+			  org.apache.hadoop.mapred.JobConf ign1,
+			  org.apache.hadoop.mapred.Reporter ign2) throws IOException {
+				throw new UnsupportedOperationException("not meant to be called");
+			}
+		  };
 		return ifmtMrv1.getSplits(arg0, arg1);
 	}
 
