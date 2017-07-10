fbbd83aeae8c433d97f607e1bef5792d9413774f
Randall Whitman <rwhitman@esri.com>
2017-07-10 14:50:43 -0700

    JSON dependencies refresh - Geometry-2.0

--- json/src/main/java/com/esri/json/EsriFeatureClass.java
+++ json/src/main/java/com/esri/json/EsriFeatureClass.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.JsonParser;
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-import org.codehaus.jackson.map.JsonMappingException;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 import com.esri.core.geometry.Geometry;
 import com.esri.core.geometry.SpatialReference;
--- json/src/main/java/com/esri/json/EsriJsonFactory.java
+++ json/src/main/java/com/esri/json/EsriJsonFactory.java
@@ -3,15 +3,15 @@ package com.esri.json;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.codehaus.jackson.JsonFactory;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.JsonParser;
-import org.codehaus.jackson.JsonProcessingException;
-import org.codehaus.jackson.Version;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.module.SimpleModule;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import com.esri.core.geometry.Geometry;
 import com.esri.core.geometry.SpatialReference;
