ba0e6f9f01a5c581753c0608d609f1871218d2ea
Randall Whitman <rwhitman@esri.com>
2026-06-17 15:57:28 -0700

    Update EsriFeature.java

--- json/src/main/java/com/esri/json/EsriFeature.java
+++ json/src/main/java/com/esri/json/EsriFeature.java
@@ -38,7 +38,7 @@ public class EsriFeature {
 	}
 	
 	/**
-	 * @deprecated
+	 * @deprecated InputStream
 	 * @param JsonParser parser (Jackson-2) pointed at the root of the JSON file created by ArcGIS
 	 * @return EsriFeature instance that describes the fully parsed JSON representation
 	 * @throws JsonParseException
